c63d7162b60941a597e2121d4c9f4d2d280c8528
tomce <tomce@192.168.0.14>
2021-01-22 15:53:51 +0100

    v1

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package fr.declaration.variable;
+
+/**
+ * @author tomce
+ *
+ */
+public class DeclarationApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte postal = 92;
+		short street = 350;
+		int id = 55050;
+		id = id + street ;
+		long physical = 2_000_000_000L;
+		float pricePromotion = 58.5F;
+		double price = 5324.21;
+		char uniqueCharactere = 'a';
+		boolean verification = true;
+		String test = "Test test";
+		
+		String randomString = "Voici le résultat d’un calcul :\n1+5=6";
+		
+		System.out.println(randomString);
+	
+
+	}
+
+}
