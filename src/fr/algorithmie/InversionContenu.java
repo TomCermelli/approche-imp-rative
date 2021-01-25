0a3ce8d6f995a1123d8384dfaff3b608689da249
tomce <tomce@192.168.0.14>
2021-01-25 11:43:53 +0100

    "Inversion Contenu"

---
+++ src/fr/algorithmie/InversionContenu.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package fr.algorithmie;
+
+/**
+ * @author tomce
+ *
+ */
+public class InversionContenu {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
+		int j = array.length-1; // On utilise la longueur du tableau pour ensuite décrémenté par la suite
+		
+		
+		int[] arrayCopy = new int[17];
+		// On ajoute les éléments de array dans arrayCopy mais dans l'odre inverse du tableau array
+		for(int i=0; i<array.length; i++) {
+			
+			arrayCopy[i]=array[j];
+			System.out.println("array est égale à : " + array[i]);
+			System.out.println(" arrayCopy est égale à : " + arrayCopy[i]);
+			System.out.println("----");
+			j--;
+			
+		}
+		
+		
+	}
+
+}
