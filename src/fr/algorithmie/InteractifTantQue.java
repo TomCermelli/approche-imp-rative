e4bc5d9ec6cb52bccceddf2aca68214fea1e5cc1
tomce <tomce@192.168.0.14>
2021-01-25 16:48:54 +0100

    "Interactif Tant que"

---
+++ src/fr/algorithmie/InteractifTantQue.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package fr.algorithmie;
+
+import java.util.Scanner;
+
+/**
+ * @author tomce
+ *
+ */
+public class InteractifTantQue {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Veuillez saisir un nombre");
+		int nb = scanner.nextInt();
+		
+		while(nb < 1 || nb > 10) {
+			System.out.println("Veuillez saisir un nombre entre 1 et 10");
+			nb = scanner.nextInt(); 
+		}
+		System.out.println("Vous avez choisis le chiffre : " + nb);
+	}
+}
