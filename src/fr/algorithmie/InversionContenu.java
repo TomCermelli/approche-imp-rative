13b7524b7c0ebbfc80c5bba23ef14888aecb813e
tomce <tomce@192.168.0.14>
2021-01-25 12:12:03 +0100

    "Recherche Max/Min"

--- src/fr/algorithmie/InversionContenu.java
+++ src/fr/algorithmie/InversionContenu.java
@@ -29,8 +29,6 @@ public class InversionContenu {
 			j--;
 			
 		}
-		
-		
 	}
 
 }
---
+++ src/fr/algorithmie/RechercheMin.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package fr.algorithmie;
+
+/**
+ * @author tomce
+ *
+ */
+public class RechercheMin {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4,};
+		int min = 0;
+		
+		//Recherche le nombre le plus grand du tableau
+		for(int i = 0; i<array.length; i++) {
+			if(array[i]<min) {
+				min = array[i];
+			}	
+		}
+		System.out.println("Le plus petit nombre de ce tableau est : " + min);
+	}
+
+}
