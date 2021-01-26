09d63d63aaa3ea6ad9771a109369adc33eae4891
tomce <tomce@192.168.0.14>
2021-01-26 18:09:17 +0100

    "First Last"

--- src/fr/algorithmie/FirstLast6.java
+++ src/fr/algorithmie/FirstLast6.java
@@ -13,10 +13,10 @@ public class FirstLast6 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int[] array = {};
+		int[] array = {6 , 1, 2};
 		boolean verif = true;
 		
-		if(array.length>0 && (array[0] == 6 && array[array.length-1] == 6)) {
+		if(array.length>0 && (array[0] == 6 || array[array.length-1] == 6)) {
 			verif = true;
 		}
 		else {
